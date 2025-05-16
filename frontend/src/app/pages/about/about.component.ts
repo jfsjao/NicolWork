2cd46e5cf100e11baa5f3e943005b541be1817c1
joaofelipes <joao.felipe@alfaebeto.org.br>
2025-05-16 16:33:35 -0300

    about ok

--- frontend/src/app/pages/about/about.component.ts
+++ frontend/src/app/pages/about/about.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CountUpModule } from 'ngx-countup';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, CountUpModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  clients = [
+    { logo: 'logo-nike-256.png', name: 'Marca 1' },
+    { logo: 'logo-nike-256.png', name: 'Marca 2' },
+    { logo: 'logo-nike-256.png', name: 'Marca 3' },
+    { logo: 'logo-nike-256.png', name: 'Marca 4' },
+    { logo: 'logo-nike-256.png', name: 'Marca 5' },
+  ];
+
+  // Valores para as animações
+  stats = {
+    years: 1,
+    clients: 5,
+    engagements: 10,
+    satisfaction: 100
+  };
+
+  getImagePath(logo: string): string {
+    return `assets/images/logos/${logo}`;
+  }
 
 }
