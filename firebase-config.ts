4dbec3bbf6763ebc3085526aff99fc31337fa859
jf <joaofelipe0143@gmail.com>
2026-03-18 14:10:52 -0300

    so deixa firebase auth

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,23 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; 
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAzRuN1rYYgjwml3bkzhLw8ujlM8I8x9Xs",
   authDomain: "nicolwork-1.firebaseapp.com",
   projectId: "nicolwork-1",
-  storageBucket: "nicolwork-1.firebasestorage.app",
   messagingSenderId: "213172189454",
-  appId: "1:213172189454:web:006053d67129f1890f3f5f",
-  measurementId: "G-FMEDQXDLZF"
+  appId: "1:213172189454:web:006053d67129f1890f3f5f"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-let analytics;
-if (typeof window !== 'undefined') {
-  analytics = getAnalytics(app);
-}
-
-export { app, auth, analytics };
+export { app, auth };
